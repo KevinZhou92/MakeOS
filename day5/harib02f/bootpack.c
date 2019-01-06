db2261c5187fc0d49ec4bbe7e8e87da9fa9b3c50
KevinZhou92 <monsterzpc@gmail.com>
2019-01-06 14:14:38 -0500

    Update

--- day5/harib02f/bootpack.c
+++ day5/harib02f/bootpack.c
@@ -125,24 +125,23 @@ void init_screen(char *vram, int x, int y)
 void putfont8(char *vram, int xsize, int x, int y, char c, char *font)
 {
 	int i;
-	char *p, d /* data */;
 	for (i = 0; i < 16; i++) {
-		p = vram + (y + i) * xsize + x;
+		char *p = vram + xsize * (y + i) + x;
 		d = font[i];
-		if ((d & 0x80) != 0) { p[0] = c; }
-		if ((d & 0x40) != 0) { p[1] = c; }
-		if ((d & 0x20) != 0) { p[2] = c; }
-		if ((d & 0x10) != 0) { p[3] = c; }
-		if ((d & 0x08) != 0) { p[4] = c; }
-		if ((d & 0x04) != 0) { p[5] = c; }
-		if ((d & 0x02) != 0) { p[6] = c; }
-		if ((d & 0x01) != 0) { p[7] = c; }
+		if ((d & 0x80) != 0) p[0] = c;
+		if ((d & 0x40) != 0) p[1] = c;
+		if ((d & 0x20) != 0) p[2] = c;
+		if ((d & 0x10) != 0) p[3] = c;
+		if ((d & 0x08) != 0) p[4] = c;
+		if ((d & 0x04) != 0) p[5] = c;
+		if ((d & 0x02) != 0) p[6] = c;
+		if ((d & 0x01) != 0) p[7] = c;
 	}
 	return;
 }
 
 void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s)
-{
+{	
 	extern char hankaku[4096];
 	for (; *s != 0x00; s++) {
 		putfont8(vram, xsize, x, y, c, hankaku + *s * 16);
