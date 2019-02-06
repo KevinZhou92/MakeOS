e7073f6a861630d233882429728e5422a9cb0cff
KevinZhou92 <monsterzpc@gmail.com>
2019-02-06 01:08:26 -0500

    Update

--- day13/harib10c/bootpack.c
+++ day13/harib10c/bootpack.c
@@ -77,7 +77,7 @@ void HariMain(void)
 	for (;;) {
 		count++;
 		sprintf(s, "counting %d", count);
-		putfonts8_asc_sht(sht_back, 0, 50, COL8_FFFFFF, COL8_008484, s, 20);
+		putfonts8_asc_sht(sht_back, 0, 150, COL8_FFFFFF, COL8_008484, s, 20);
 
 		io_cli();
 		if (fifo8_status(&keyfifo) + fifo8_status(&mousefifo) + fifo8_status(&timerfifo) == 0) {
