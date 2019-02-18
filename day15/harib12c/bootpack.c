4eabeac726e0d1d8e922d53c74967444d61af10a
KevinZhou92 <monsterzpc@gmail.com>
2019-02-18 18:55:38 -0500

    Update

--- day15/harib12c/bootpack.c
+++ day15/harib12c/bootpack.c
@@ -296,7 +296,7 @@ void task_b_main(void)
 	timer_init(timer_ts, &fifo, 1);
 	timer_settime(timer_ts, 2);
 
-	struct SHEET *sht_back = (struc *SHEET)*((int *) 0x0eec
+	struct SHEET *sht_back = (struc *SHEET)*((int *) 0x0eec;
 
 	for (;;) {
 		io_cli();
