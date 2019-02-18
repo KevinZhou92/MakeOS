2696da67110d74c863e6f1ff3e1dda097f898205
KevinZhou92 <monsterzpc@gmail.com>
2019-02-18 18:53:42 -0500

    Update

--- day15/harib12c/bootpack.c
+++ day15/harib12c/bootpack.c
@@ -29,6 +29,7 @@ void HariMain(void)
 	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
 	struct SHTCTL *shtctl;
 	struct SHEET *sht_back, *sht_mouse, *sht_win;
+	*((int *) 0x0eec ) = (int) sht_back;
 	unsigned char *buf_back, buf_mouse[256], *buf_win;
 	static char keytable[0x54] = {
 		0,   0,   '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '^', 0,   0,
