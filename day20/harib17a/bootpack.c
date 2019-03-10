c6868a99cf6aeeaa9beb785f1c0d3d287412ea58
KevinZhou92 <monsterzpc@gmail.com>
2019-03-10 15:20:39 -0400

    Update

--- day20/harib17a/bootpack.c
+++ day20/harib17a/bootpack.c
@@ -138,10 +138,6 @@ void HariMain(void)
 
 			char temp[2];
 			if (256 <= i && i <= 511) { /* キーボードデータ */
-				sprintf(temp, "%02X", i - 256);
-				putfonts8_asc_sht(sht_win, cursor_x, 28, COL8_000000, COL8_FFFFFF, temp, 2);
-				cursor_x += 16;
-
 				if (i < 0x80 + 256) { /* キーコードを文字コードに変換 */
 					if (key_shift == 0) {
 						s[0] = keytable0[i - 256];
