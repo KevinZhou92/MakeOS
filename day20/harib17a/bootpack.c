47b87590fadecd66276a19e00b1d93120b51da4d
KevinZhou92 <monsterzpc@gmail.com>
2019-03-10 15:04:03 -0400

    Update

--- day20/harib17a/bootpack.c
+++ day20/harib17a/bootpack.c
@@ -134,9 +134,6 @@ void HariMain(void)
 			io_sti();
 		} else {
 			i = fifo32_get(&fifo);
-			char s[2];
-			sprintf(s, "%02X", i-256);
-			putfonts8_asc_sht(sht_win, cursor_x, 28, COL8_000000, COL8_FFFFFF, s, 2);
 			io_sti();
 			if (256 <= i && i <= 511) { /* キーボードデータ */
 				if (i < 0x80 + 256) { /* キーコードを文字コードに変換 */
