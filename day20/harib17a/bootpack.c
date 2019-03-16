467f8120062f3423d83e820ee5aaca9a7c7c5e52
KevinZhou92 <monsterzpc@gmail.com>
2019-03-16 18:56:02 -0400

    Update

--- day20/harib17a/bootpack.c
+++ day20/harib17a/bootpack.c
@@ -145,6 +145,8 @@ void HariMain(void)
 						s[0] = keytable1[i - 256];
 					}
 					sprintf(temp, "%02X", i)
+					putfonts8_asc_sht(sht_win, cursor_x, 28, COL8_000000, COL8_FFFFFF, temp, 2);
+					cursor_x +=16;
 				} else {
 					s[0] = 0;
 				}
