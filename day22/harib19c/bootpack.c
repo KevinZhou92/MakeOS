0dea0030d24cc51621149183af850bd47fcc130f
KevinZhou92 <monsterzpc@gmail.com>
2019-03-16 20:08:02 -0400

    Update

--- day22/harib19c/bootpack.c
+++ day22/harib19c/bootpack.c
@@ -231,7 +231,7 @@ void HariMain(void)
 					fifo32_put(&keycmd, KEYCMD_LED);
 					fifo32_put(&keycmd, key_leds);
 				}
-				if (i == 256 + 99 && key_shift != 0 && task_cons->tss.ss0 != 0) {	/* Shift+F1 */
+				if (i == 256 + 0x63 && key_shift != 0 && task_cons->tss.ss0 != 0) {	/* Shift+F1 */
 					cons = (struct CONSOLE *) *((int *) 0x0fec);
 					cons_putstr0(cons, "\nBreak(key) :\n");
 					io_cli();	/* 強制終了処理中にタスクが変わると困るから */
