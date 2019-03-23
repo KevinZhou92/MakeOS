bbbd709e3596769c33f41c4d4d23d7f033b68894
KevinZhou92 <monsterzpc@gmail.com>
2019-03-22 23:01:05 -0400

    Update

--- day24/harib21a/bootpack.c
+++ day24/harib21a/bootpack.c
@@ -234,7 +234,7 @@ void HariMain(void)
 					task_cons->tss.eip = (int) asm_end_app;
 					io_sti();
 				}
-				if (i == 256 + 0x57 && shtctl->top > 2) {	/* F11 */
+				if (i == 256 + 0x1D && shtctl->top > 2) {	/* F11 */
 					sheet_updown(shtctl->sheets[1], shtctl->top - 1);
 				}
 				if (i == 256 + 0xfa) {	/* キーボードがデータを無事に受け取った */
